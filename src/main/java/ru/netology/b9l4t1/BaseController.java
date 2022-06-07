be4ab5c54f2bca7c9db5c0d048e728518f1ffe34
younguru <kiosun@mail.ru>
2022-06-07 16:41:27 +0300

    done

--- src/main/java/ru/netology/b9l4t1/BaseController.java
+++ src/main/java/ru/netology/b9l4t1/BaseController.java
@@ -1,11 +1,14 @@
 package ru.netology.b9l4t1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,19 +19,21 @@ public class BaseController {
     BaseRepository baseRepository;
 
     @GetMapping("/by-city")
+    @Secured("ROLE_READ")
     public List<Person> getPersonByCity(@RequestParam(name = "city") String city) {
         return baseRepository.getPersonsByCity(city);
     }
 
     @GetMapping("/by-age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> getPersonByAge(@RequestParam(name = "age") Integer age) {
         return baseRepository.getPersonsByAge(age);
     }
 
     @GetMapping("/by-ns")
+    @PreAuthorize("#name == authentication.principal.username and (hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE'))")
     public Optional<List<Person>> getPersonByNS(@RequestParam(name = "name") String name,
                                                 @RequestParam(name = "surname") String surname) {
         return baseRepository.getPersonsByNS(name, surname);
     }
-
 }
--- src/main/java/ru/netology/b9l4t1/BaseSecurity.java
+++ src/main/java/ru/netology/b9l4t1/BaseSecurity.java
@@ -2,20 +2,16 @@ package ru.netology.b9l4t1;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class BaseSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Vasya").password("{noop}1234").roles("ADMIN");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().and()
-                .authorizeRequests().antMatchers("/by-city").permitAll()
-                .and().authorizeRequests().anyRequest().hasAuthority("ROLE_ADMIN");
+        auth.inMemoryAuthentication().withUser("Vasya").password("{noop}1234").roles("READ");
+        auth.inMemoryAuthentication().withUser("Alexey").password("{noop}123").roles("READ", "WRITE");
+        auth.inMemoryAuthentication().withUser("Ilya").password("{noop}234").roles("DELETE");
     }
 }
